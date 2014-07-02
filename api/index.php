dba7db859c4182cab3d674707709cda9d5bc04ac
Adrian Kosmaczewski <akosma@me.com>
2014-07-02 19:01:59 +0200

    Ups, wrong path

--- api/index.php
+++ api/index.php
@@ -33,7 +33,7 @@ $app = new \Slim\Slim();
 // Main program starts here
 $pipe = dirname(__FILE__) . "/../fifo";
 $sound_setting_file = dirname(__FILE__) . "/../sound_setting";
-$basedir = "/mnt/usbdrive/videos/";
+$basedir = "/media/usb1/videos";
 
 $get_disk = function () {
 	$response = [
